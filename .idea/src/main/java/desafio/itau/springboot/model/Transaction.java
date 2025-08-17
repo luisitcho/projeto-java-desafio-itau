735225988c4fcd9cad0694559d24332c744d5496
luisitcho <luishenrisc1@gmail.com>
2025-08-17 16:33:50 -0300

    projeto desafio itau - update

--- .idea/src/main/java/desafio/itau/springboot/model/Transaction.java
+++ .idea/src/main/java/desafio/itau/springboot/model/Transaction.java
@@ -1,4 +1,21 @@
-package model;
+package desafio.itau.springboot.model;
+
+import java.time.OffsetDateTime;
 
 public class Transaction {
+    private double valor;
+    private OffsetDateTime dataHora;
+
+    public Transaction(OffsetDateTime dataHora, double valor) {
+        this.dataHora = dataHora;
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
 }
